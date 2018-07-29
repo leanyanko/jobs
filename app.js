91df896151b24a47819d56317ec7ec8ab09f6c98
mrs.leonenko <mrs.leonenko@gmail.com>
2018-07-28 22:35:20 -0400

    passport works

--- app.js
+++ app.js
@@ -37,43 +37,13 @@ app.use('/users', users);
 function auth (req, res, next) {
  console.log(req.session);
 
-// if (!req.session.user) {
   if (!req.user) {
-  //  var authHeader = req.headers.authorization;
-  //   if (!authHeader) {
         var err = new Error('You are not authenticated!');
-       // res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 403;
         return next(err);
-       // return;
     }
-
-    // var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    // var user = auth[0];
-    // var pass = auth[1];
-    // if (user == 'admin' && pass == 'password') {
-    //  // res.cookie('user', 'admin', {signed: true});
-    //  req.session.user = 'admin';
-    //     next(); // authorized
-    // } else {
-    //     var err = new Error('You are not authenticated!');
-    //     res.setHeader('WWW-Authenticate', 'Basic');      
-    //     err.status = 401;
-    //     next(err);
-    // }
-  // }
   else {
-     // if (req.signedCookies.user === 'authenticated') {
-  //    if (req.session.user === 'authenticated') {
         next();
-    //   } 
-    //   else {
-    //     var err = new Error('You are not authenticated!');
-    //   //  res.setHeader('WWW-Authenticate', 'Basic');
-    //     err.status = 403;
-    //     return next(err);
-    //  // }
-    // }
   }
 }
 
