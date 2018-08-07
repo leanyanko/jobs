16c79df771b81587e8a0b64b88bff24e4a72bf91
mrs.leonenko <mrs.leonenko@gmail.com>
2018-08-07 16:03:02 -0400

    cors continue

--- routes/cors.js
+++ routes/cors.js
@@ -1,7 +1,28 @@
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const whitelist = ['http://localhost:3000', 'https://essteem.herokuapp.com/'];
+// var corsOptionsDelegate = (req, callback) => {
+//     var corsOptions;
+//     console.log(req.header('Origin'));
+//     if(whitelist.indexOf(req.header('Origin')) !== -1) {
+//         corsOptions = { origin: true };
+//     }
+//     else {
+//         corsOptions = { origin: false };
+//     }
+
+//     callback(null, corsOptions);
+// }
+
+// exports.cors = cors();
+// exports.corsWithOptions = cors(corsOptionsDelegate);
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const whitelist = ['http://localhost:3000', 'https://essteem.herokuapp.com/'];
+
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'https://essteem.herokuapp.com/', 'https://essteem.herokuapp.com'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
@@ -11,9 +32,8 @@ var corsOptionsDelegate = (req, callback) => {
     else {
         corsOptions = { origin: false };
     }
-
     callback(null, corsOptions);
-}
+};
 
 exports.cors = cors();
 exports.corsWithOptions = cors(corsOptionsDelegate);
