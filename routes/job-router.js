f7250c7c47494fc382401f2ec2d5af7c98aefb7b
mrs.leonenko <mrs.leonenko@gmail.com>
2018-08-07 15:41:22 -0400

    cors beginning

--- routes/job-router.js
+++ routes/job-router.js
@@ -1,63 +1,73 @@
 var express = require("express");
 const authenticate = require('../authenticate');
 
+var cors = require('./cors');
+
 var routes = function(Job) {
-  var jobRouter = express.Router();
+    var jobRouter = express.Router();
+    var jobController = require("../controllers/jobController")(Job);
 
-  var jobController = require("../controllers/jobController")(Job);
-  jobRouter
-    .route("/")
-    .post(authenticate.verifyUser, jobController.post)
-    .get(jobController.get)
-    .delete(authenticate.verifyUser, function(req, res) {
-      Job.find(function(err, jobs) {});
-    });
+    jobRouter
+        .route("/")
+        .options(cors.corsWithOptions, (req, res) => {
+          res.sendStatus(200);
+        })
 
-  jobRouter.use("/:jobId", function(req, res, next) {
-    Job.findById(req.params.jobId, function(err, job) {
-      if (err) res.status(500).send(err);
-      else if (job) {
-        req.job = job;
-        next();
-      } else {
-        res.status(404).send("no job found");
-      }
-    });
-  });
+        .post(cors.corsWithOptions, authenticate.verifyUser, jobController.post)
+        .get(cors.cors, jobController.get)
+        .delete(cors.corsWithOptions, authenticate.verifyUser, function(req, res) {
+          Job.find(function(err, jobs) {});
+        });
 
-  jobRouter
-    .route("/:jobId")
-    .get(function(req, res) {
-      res.json(req.job);
-    })
-    .put(authenticate.verifyUser, function(req, res) {
-      req.job.title = req.body.title;
-      req.job.text = req.body.text;
-      if (req.body.images) req.job.images.push(req.body.images);
-
-      req.job.save(function(err) {
-        if (err) res.status(500).send(err);
-        else res.json(req.job);
+    jobRouter.use("/:jobId", function(req, res, next) {
+        Job.findById(req.params.jobId, function(err, job) {
+          if (err) res.status(500).send(err);
+          else if (job) {
+            req.job = job;
+            next();
+          } else {
+            res.status(404).send("no job found");
+          }
+        });
       });
-      res.json(req.job);
-    })
-    .patch(authenticate.verifyUser, function(req, res) {
-      if (req.body._id) delete req.body._id;
-      for (p in req.body) {
-        req.job[p] = req.body[p];
-      }
-      req.job.save(function(err) {
-        if (err) res.status(500).send(err);
-        else res.json(req.job);
-      });
-    })
-    .delete(authenticate.verifyUser, function(req, res) {
-      req.job.remove(function(err) {
-        if (err) res.status(500).send(err);
-        else res.status(204).send("removed");
-      });
-    });
-  return jobRouter;
+
+    jobRouter
+      .route("/:jobId")
+      .options(cors.corsWithOptions, (req, res) => {
+          res.sendStatus(200);
+        })
+
+      .get(cors.cors, function(req, res) {
+          res.json(req.job);
+        })
+      .put(cors.corsWithOptions, authenticate.verifyUser, function(req, res) {
+          req.job.title = req.body.title;
+          req.job.text = req.body.text;
+          if (req.body.images) req.job.images.push(req.body.images);
+
+          req.job.save(function(err) {
+            if (err) res.status(500).send(err);
+            else res.json(req.job);
+          });
+          res.json(req.job);
+        })
+      .patch(cors.corsWithOptions, authenticate.verifyUser, function(req, res) {
+          if (req.body._id) delete req.body._id;
+          for (p in req.body) {
+            req.job[p] = req.body[p];
+          }
+          req.job.save(function(err) {
+            if (err) res.status(500).send(err);
+            else res.json(req.job);
+          });
+        })
+      .delete(cors.corsWithOptions, authenticate.verifyUser, function(req, res) {
+          req.job.remove(function(err) {
+            if (err) res.status(500).send(err);
+            else res.status(204).send("removed");
+          });
+        });
+      return jobRouter;
 };
 
 module.exports = routes;
